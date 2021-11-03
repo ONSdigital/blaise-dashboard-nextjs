095492bf18336e51450208fdef1a5c9f5bae864b
elthorne <46898565+elthorne@users.noreply.github.com>
2021-11-03 15:51:58 +0000

    Need to cd in to the main dir

--- ci/tasks/deploy/task.sh
+++ ci/tasks/deploy/task.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 echo "${GOOGLE_CREDENTIALS}" >/tmp/google_creds.json
 gcloud auth activate-service-account --project="${GCP_PROJECT}" --key-file=/tmp/google_creds.json
 
+cd dashboard-git
+
 gcloud app deploy
