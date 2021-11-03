5488af0290c49df0c1049309335f53dddc224377
elthorne <46898565+elthorne@users.noreply.github.com>
2021-11-03 15:42:59 +0000

    Added deploy pipeline stuff and things

---
+++ ci/tasks/deploy/task.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "${GOOGLE_CREDENTIALS}" >/tmp/google_creds.json
+gcloud auth activate-service-account --project="${GCP_PROJECT}" --key-file=/tmp/google_creds.json
+
+gcloud app deploy
