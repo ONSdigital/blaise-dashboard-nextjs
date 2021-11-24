b4677ea3ffc1a437fde590b63d6711d81e578b32
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2021-11-24 16:41:04 +0000

    Changes by Matthew Palmer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,24 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 // reactstrap components
-import { Alert } from "reactstrap";
+import Health from "./health";
 
 
 
 const Home: NextPage = () => {
+    const [gifUrl, setGifUrl] = useState<string>("")
+    useEffect(() => {
+        fetch(giphy).then((response) => {
+            response.json().then((data) => {
+                setGifUrl(data.data[Math.floor((Math.random() * 100))].id)
+            })
+        })
+    }, [])
+
+
     return (
         <div className={styles.container}>
             <Head>
@@ -17,11 +27,13 @@ const Home: NextPage = () => {
                 <meta name="description" content="Blaise dashboard"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <Alert color="success">
-                <strong>Success!</strong> Blaise is up!
-            </Alert>
+            <Health/>
+            {
+                gifUrl !== "" && <img src={`https://media0.giphy.com/media/${gifUrl}/giphy.gif`}/>
+            }
         </div>
     )
 }
 
 export default Home
+
